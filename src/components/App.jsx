255f6d7b018bda6b32571684fb660d1abeafc119
Kelechi Kizito Ugwu <111236979+Kelechikizito@users.noreply.github.com>
2023-06-29 04:51:11 -0700

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ClassComponent from "./ClassComponent";
+
+class App extends React.Component {
+  render() {
+    return <ClassComponent />;
+  }
+}
+
+export default App;
